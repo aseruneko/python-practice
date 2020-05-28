17e749a136767fa3b17fc7be3833e0318c9ad5d9
aseruneko <aseruneko@gmail.com>
2020-05-28 15:36:23 +0900

    lesson27

---
+++ Python3 入門/lesson27.py
@@ -0,0 +1,11 @@
+sales = {"taguchi": 200, "fkoji" : 400}
+
+print(sales["taguchi"])
+sales["taguchi"] = 300
+print(sales["taguchi"])
+sales["dotinstall"] = 500
+del(sales["fkoji"])
+print(sales)
+
+for key, value in sales.items():
+    print("{0}:{1}".format(key,value))
