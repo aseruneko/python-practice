3409a3e6c295f783409a982391828c3ef91a8db8
aseruneko <aseruneko@gmail.com>
2020-05-28 15:46:14 +0900

    lesson31

---
+++ Python3 入門/lesson31.py
@@ -0,0 +1,9 @@
+# comprehension
+
+# 0 - 9
+print([i for i in range(10)])
+print([i * 3 for i in range(10)])
+print([i * 3 for i in range(10) if i % 2 == 0])
+print((i * 3 for i in range(10) if i % 2 == 0))
+print(i * 3 for i in range(10) if i % 2 == 0)
+print({i * 3 for i in range(10) if i % 2 == 0})
