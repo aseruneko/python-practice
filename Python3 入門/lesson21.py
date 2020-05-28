8cf05ba360508f2ca55b2e4058db14b6a7c1e7f8
aseruneko <aseruneko@gmail.com>
2020-05-28 15:20:48 +0900

    lesson21

--- Python3 入門/lesson21.py
+++ Python3 入門/lesson21.py
@@ -1,6 +1,7 @@
-import mypackage.user
+# import mypackage.user as mymodule
+from mypackage.user import AdminUser
 
-bob = mypackage.user.AdminUser("bob", 23)
+bob = AdminUser("bob", 23)
 
 print(bob.name)
 bob.say_hi()
