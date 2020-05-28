801472080384e60f5d8cdeb7d3af0b91e43994c9
aseruneko <aseruneko@gmail.com>
2020-05-28 15:06:23 +0900

    lesson17

---
+++ Python3 入門/lesson17.py
@@ -0,0 +1,13 @@
+# class
+# access limitation
+
+class User:
+    def __init__(self, name):
+        self.__name = name
+    def say_hi(self):
+        print("hi {0}".format(self.__name))
+
+tom = User("tom")
+
+# print(tom.__name)
+tom.say_hi()
