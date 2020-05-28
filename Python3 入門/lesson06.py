9e57411c49aea1d0abe25d833752270b625bb60f
aseruneko <aseruneko@gmail.com>
2020-05-28 14:43:52 +0900

    lesson06

---
+++ Python3 入門/lesson06.py
@@ -0,0 +1,9 @@
+name = "Taguchi"
+score = 52.8
+
+print("name: %s, score: %f" %(name,score))
+print("name: %-10s, score: %10.2f" %(name,score))
+
+print("name: {0}, score: {1}".format(name,score))
+print("name: {0:10s}, score: {1:10.2f}".format(name,score))
+print("name: {0:>10s}, score: {1:<10.2f}".format(name,score))
