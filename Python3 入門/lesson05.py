3be5fc14e90ec47245dd4defd55725bb452b42ae
aseruneko <aseruneko@gmail.com>
2020-05-28 14:41:22 +0900

    lesson05

---
+++ Python3 入門/lesson05.py
@@ -0,0 +1,21 @@
+# + - * / // % **
+
+x = 10
+print(x / 3)
+print(x // 3)
+print(x % 3)
+print(x ** 2)
+
+y = 4
+y = y + 12
+print(y)
+
+y += 12
+print(y)
+
+print(True and False)
+print(True or False)
+print(not True)
+
+print("Hello " + "World")
+print("Hello" * 3)
