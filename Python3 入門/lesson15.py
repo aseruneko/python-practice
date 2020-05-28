93c7490a5c86c4a088ad172e547a2c90a884cd22
aseruneko <aseruneko@gmail.com>
2020-05-28 15:01:52 +0900

    lesson15

---
+++ Python3 入門/lesson15.py
@@ -0,0 +1,13 @@
+class User:
+    # class variable
+    count = 0
+    def __init__(self, name):
+        User.count += 1
+        self.name = name
+
+print(User.count)
+tom = User("tom")
+bob = User("bob")
+print(User.count)
+
+print(tom.count)
