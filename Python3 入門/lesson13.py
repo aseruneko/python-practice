6703d8e8db6ea0c1eb9ce75a9958d7d08a011511
aseruneko <aseruneko@gmail.com>
2020-05-28 14:59:13 +0900

    lesson13

---
+++ Python3 入門/lesson13.py
@@ -0,0 +1,18 @@
+# class
+
+# user_name = "taguchi"
+# user_score = 10
+
+class User:
+    pass
+
+tom = User() # instance
+tom.name = "tom"
+tom.score = 20
+
+bob = User()
+bob.name = "bob"
+bob.level = 5
+
+print(tom.name)
+print(bob.level)
