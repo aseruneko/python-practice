e75cc1a9cb568b5fdd77e09ab7a10f37f853f5f6
aseruneko <aseruneko@gmail.com>
2020-05-28 14:51:51 +0900

    lesson09

---
+++ Python3 入門/lesson09.py
@@ -0,0 +1,13 @@
+# for
+
+# for varibale in data:
+# # execution
+
+# for i in range(0, 10):
+for i in range(10):
+    if i == 5:
+        #break
+        continue
+    print(i)
+else:
+    print("end")
