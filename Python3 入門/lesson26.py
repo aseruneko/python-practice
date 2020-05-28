7352069dad52b9ddf7786bea626f039c095ad609
aseruneko <aseruneko@gmail.com>
2020-05-28 15:33:20 +0900

    lesson26

---
+++ Python3 入門/lesson26.py
@@ -0,0 +1,17 @@
+a = set([5,3,8,5])
+b = {5, 3, 8, 5}
+
+print(a)
+print(b)
+print(5 in a) # True
+a.add(2)
+a.remove(3)
+
+print(a)
+print(len(a))
+
+a = {1,3,5,8}
+b = {3,5,8,9}
+print(a | b)
+print(a & b)
+print(a - b)
